5e2ff269fc514d41c013471670f72084f51f0cbd
Bhargav Kaushik <kaushikbhargav786@gmail.com>
2025-05-08 00:32:59 +0530

    Added method to find employee by ID in EmployeeController

--- src/main/java/com/employee/employeedata/controller/EmployeeController.java
+++ src/main/java/com/employee/employeedata/controller/EmployeeController.java
@@ -1,9 +1,12 @@
 package com.employee.employeedata.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.employee.employeedata.model.Employee;
@@ -18,5 +21,12 @@ public class EmployeeController {
 	@GetMapping("/employee")
 	public List<Employee> getAllEmployees(){
 		return repo.findAll();
+	}
+	
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable int id) {
+	    Optional<Employee> employee = repo.findById(id);
+	    return employee.map(ResponseEntity::ok)
+	                   .orElseGet(() -> ResponseEntity.notFound().build());
 	}
 }
